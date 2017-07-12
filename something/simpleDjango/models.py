d5a90aab80325e06d6f9ddc755f5900920b05380
Dudov Dima <dudov.dm@gmail.com>
2017-07-13 00:02:05 +0300

    fix models

--- something/simpleDjango/models.py
+++ something/simpleDjango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
@@ -10,14 +11,17 @@ class Article(models.Model):
     date = models.DateTimeField(verbose_name='Дата')
     is_index = models.BooleanField(verbose_name='Индексная?')
 
+    def clean(self):
+        if self.is_index and Article.objects.filter(catalog=self.catalog, is_index=True).exclude(id=self.id).exists():
+            raise ValidationError('This catalog have index page yet')
+
     def __str__(self):
-        return str(self.catalog) + ' --> ' + self.title
+        return self.title
 
 
 class Catalog(models.Model):
-    parent_catalog = models.ForeignKey('Catalog', verbose_name='Родительский каталог',
-                                       default='/', blank=True, null=True)
+    parent_catalog = models.ForeignKey('Catalog', verbose_name='Родительский каталог', blank=True, null=True)
     title = models.CharField(max_length=200, verbose_name='Название')
 
     def __str__(self):
-        return str(self.parent_catalog) + ' --> ' + self.title
+        return '{} --> {}'.format(str(self.parent_catalog), self.title)
